c92cd90dca9a9e2e7d56cf86768490329babac31
07MAXQ <msdurov070@gmail.com>
2025-10-22 14:10:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import os
-from telegram.ext import ApplicationBuilder, CommandHandler
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from dotenv import load_dotenv
 
+load_dotenv()  # подгружает переменные окружения (Render тоже их подставит)
 TOKEN = os.getenv("BOT_TOKEN")
 
-async def start(update, context):
-    await update.message.reply_text("Привет! Я твой бот 🎉")
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    await message.answer(f"Привет, {message.from_user.first_name}! 🎁 Добро пожаловать в Gift Royale 🖤")
+
+async def main():
+    print("Бот запущен...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
